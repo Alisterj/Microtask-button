22c209234c5d75332df8fa360240cc7a4f8c5f22
Alisterj <lisarsiss@gmail.com>
2022-03-10 15:11:14 +0300

    homework button

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,35 @@ function App() {
     const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
         console.log("Hi, I'm Dina")
     }*/
-    const onClickHandler = (name: string) => {
+    /*const onClickHandler = (name: string) => {
         console.log(name)
+    }*/
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    const foo1 = () => {
+        //Ничего не передавать, но в консоли должно показаться 100200
+        console.log(100200)
+    }
+    const foo2 = (takeNumber: number) => {
+        //Передать цифру - 100200
+        console.log(takeNumber)
     }
     return (
         <div className="App">
             {/*Microtask button lesson*/}
-            <button onClick={() => onClickHandler('Vasya')}>MyYouTubeChannel-1
+            {/*<button onClick={() => onClickHandler('Vasya')}>MyYouTubeChannel-1
             </button>
             <button onClick={() => onClickHandler('Dina')}>MyYouTubeChannel-2
             </button>
             <button onClick={() => onClickHandler('some info')}>MyYouTubeChannel-3
-            </button>
+            </button>*/}
+
+            {/*//////////////////////////////////////////////////////////////////////////////////////////////*/}
+
+            {/*Microtask button homework*/}
+            <button onClick={foo1}>1</button>
+            <button onClick={()=>foo2(100200)}>2</button>
         </div>
     );
 }
