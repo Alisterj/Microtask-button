fd3afe309ca0271c3d490c8480221fa7c336c0cc
Alisterj <lisarsiss@gmail.com>
2022-03-11 16:55:15 +0300

    update button

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import Button from "./Components/Button";
 
 function App() {
 
-    const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const Button2Foo = (subscriber: string,  age: number, address: string) => {
+    const unButton = (subscriber: string, age: number, address: string) => {
         console.log(subscriber, age, address)
     }
     const Button3Foo = () => {
@@ -23,8 +20,8 @@ function App() {
             </div>
             <p>2 part - Universal Button</p>
             <div>
-                <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('I\'m Vasya', 21, 'Lenina 122')}/>
-                <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('I\'m Ivan', 27, 'Kalinina 122')}/>
+                <Button name={'MyYouTubeChannel-1'} callBack={() => unButton('I\'m Vasya', 21, 'Lenina 122')}/>
+                <Button name={'MyYouTubeChannel-2'} callBack={() => unButton('I\'m Ivan', 27, 'Kalinina 122')}/>
                 <Button name={'Button-3'} callBack={Button3Foo}/>
             </div>
         </div>
